e6e5b367024bb9876ac4e6f234a7a9c0358b4bb8
zhangfengtao <zhangfengtao@ejiaoji.com>
2018-02-06 21:18:14 +0800

    第一次leecode

---
+++ src/repeatsubstring/RepereatSubstring.java
@@ -0,0 +1,34 @@
+package repeatsubstring;
+
+/**
+ * RepereatSubstring
+ *
+ * @author zft
+ * @date 2018/2/6
+ */
+public class RepereatSubstring {
+
+    public static int lengthOfLongestSubstring(String s) {
+        int len;
+        if (s == null || (len = s.length()) == 0) return 0;
+        int preP = 0, max = 0;
+        int[] hash = new int[128];
+        for (int i = 0; i < len; ++i) {
+            char c = s.charAt(i);
+            if (hash[c] > preP) {
+                preP = hash[c];
+            }
+            int l = i - preP + 1;
+            hash[c] = i + 1;
+            if (l > max) max = l;
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+
+        String s = "kwfgdfgh";
+        int i = lengthOfLongestSubstring(s);
+        System.out.println(i);
+    }
+}
---
+++ src/twosum/TwoSum.java
@@ -0,0 +1,67 @@
+package twosum;
+
+import java.util.HashMap;
+
+/**
+ * twosum
+ *
+ * @author zft
+ * @date 2018/2/6
+ */
+public class TwoSum {
+
+    /**
+     * Description
+     * Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+     * You may assume that each input would have exactly one solution, and you may not use the same element twice.
+     * Example:
+     * Given nums = [2, 7, 11, 15], target = 9,
+     * Because nums[0] + nums[1] = 2 + 7 = 9,
+     * return [0, 1].
+     * Tags: Array, Hash Table
+     */
+
+    private static int[] twoSum(int[] nums, int target) {
+        int[] targets = new int[2];
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    targets[0] = i;
+                    targets[1] = j;
+                    return targets;
+                }
+            }
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+        int[] a = new int[]{2, 11, 7, 15};
+        Integer target = 9;
+        int[] ints = twoSum2(a, target);
+        assert ints != null;
+        for (int anInt : ints) {
+            System.out.println("ints=" + anInt);
+        }
+    }
+
+    /**
+     * 将当前索引的所需值存储在key中，将自己索引存储在value,直到循环到所需值，取value索引.
+     * @param nums
+     * @param target
+     * @return
+     */
+    public static int[] twoSum2(int[] nums, int target) {
+        int len = nums.length;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < len; ++i) {
+            if (map.containsKey(nums[i])) {
+                return new int[]{map.get(nums[i]), i};
+            }
+            map.put(target - nums[i], i);
+        }
+        return null;
+    }
+
+
+}
