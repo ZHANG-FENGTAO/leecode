dae476fb1af01cc762708e2ed4560e611248f20d
wb-zft382553 <wb-zft382553@alibaba-inc.com>
2018-12-27 11:57:02 +0800

    leecode 167 双指针

--- src/s167/TwoSum2Solution.java
+++ src/s167/TwoSum2Solution.java
@@ -26,6 +26,27 @@ public class TwoSum2Solution {
         解释: 2 与 7 之和等于目标数 9 。因此 index1 = 1, index2 = 2 。
      */
 
+    private static int[] twoSum2(int[] numbers, int target) {
+        int[] res = new int[2];
+        int left = 0;
+        int right = numbers.length - 1;
+        while (left < right) {
+            if (numbers[left] + numbers[right] == target) {
+                res[0] = left + 1;
+                res[1] = right + 1;
+                return res;
+            } else {
+                if (numbers[left] + numbers[right] < target) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+        }
+        return null;
+    }
+
+
     private static int[] twoSum(int[] numbers, int target) {
         int[] res = new int[2];
         Map<Integer, Integer> map = new HashMap<>(numbers.length / 2 + 1);
@@ -44,7 +65,7 @@ public class TwoSum2Solution {
 
     public static void main(String[] args) {
         int[] nums = new int[]{2, 7, 11, 15, 27};
-        int[] ints = twoSum(nums, 42);
+        int[] ints = twoSum2(nums, 9);
         assert ints != null;
         Arrays.stream(ints).forEach(System.out::println);
     }
