36d3136a46a71b1dd1dcc024f85cff05fef81761
wb-zft382553 <wb-zft382553@alibaba-inc.com>
2018-12-29 11:27:45 +0800

    leecode s217、219

---
+++ src/s219/ContainsNearbyDuplicate2.java
@@ -0,0 +1,43 @@
+package src.s219;
+
+/**
+ * @author zft
+ * @date 2018/12/29.
+ */
+public class ContainsNearbyDuplicate2 {
+
+    /*
+        给定一个整数数组和一个整数 k，判断数组中是否存在两个不同的索引 i 和 j，使得 nums [i] = nums [j]，并且 i 和 j 的差的绝对值最大为 k。
+
+        示例 1:
+
+        输入: nums = [1,2,3,1], k = 3
+        输出: true
+        示例 2:
+
+        输入: nums = [1,0,1,1], k = 1
+        输出: true
+        示例 3:
+
+        输入: nums = [1,2,3,1,2,3], k = 2
+        输出: false
+     */
+
+    private static boolean containsNearbyDuplicate(int[] nums, int k) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = nums.length - 1; j > i; j--) {
+                if (nums[i] == nums[j] && j - i <= k) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{1, 0, 1, 1};
+        boolean b = containsNearbyDuplicate(nums, 2);
+        System.out.println(b);
+    }
+
+}
